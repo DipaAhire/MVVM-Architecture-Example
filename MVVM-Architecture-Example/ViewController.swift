a9c3d1a06f08bd6e7e0325cf136bcad99de67440
Dipali Ahire <ahire.dipali5@gmail.com>
2022-03-31 08:32:07 +0530

    Initial commit

---
+++ MVVM-Architecture-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Architecture-Example
+//
+//  Created by Dipali Rajbanshi on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
